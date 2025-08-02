a267df74752e0ba8e00aab6f0ad5d99713d449b2
doatgoat <e0ghost@gmail.com>
2025-08-02 11:37:54 +0500

    B: counter updated

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,11 +6,11 @@ const Counter = () => {
     return (
     <div id="container">
         <div>
-            <h1 id= 'number'>{count} </h1>
+            <h1 className= 'number'>{count} </h1>
         </div>
         <div className="btns-container">
-            <button id='increament' onClick={()=>{setCount(count+1)}}> +</button>
-            <button id='increament'  onClick={()=>{setCount(count-1)}}>-</button>
+            <button className='increment' onClick={()=>{setCount(count+1)}}> +</button>
+            <button className='increment'  onClick={()=>{setCount(count-1)}}>-</button>
         </div>
     </div>
   )
